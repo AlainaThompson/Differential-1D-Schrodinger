09a568c89a9ef8682ddd007431178b5b0678c6e3
Alaina Thompson <athompson9@hawk.iit.edu>
2022-02-18 09:42:08 -0600

    Initial Commit

---
+++ Shared/Differential_1D_SchrodingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Differential_1D_SchrodingerApp.swift
+//  Shared
+//
+//  Created by Alaina Thompson on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Differential_1D_SchrodingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
