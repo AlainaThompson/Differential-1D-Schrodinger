475580ba01e28fa40425ca09340558a7b2b03b9a
Alaina Thompson <athompson9@hawk.iit.edu>
2022-03-04 11:12:34 -0600

    1D_Schrodinger_Eq

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,16 +6,56 @@
 //
 
 import SwiftUI
+import CorePlot
+
+
+typealias plotDataType = [CPTScatterPlotField : Double]
 
 struct ContentView: View {
+    
+   
+    
+    @State var psi_String = "0.0"
+    
+    @ObservedObject var diffModel = DiffEq()
+
     var body: some View {
-        Text("Hello, world!")
+        
+        
+        VStack(alignment: .center) {
+            Text("psi")
+                .font(.callout)
+                .bold()
+            TextField("", text: $psi_String)
+                .padding()
+        }
+        
+       
+        
+        
+        
+        Button("Calculate", action: {Task.init{await self.calculateDiff()}})
             .padding()
+            .disabled(diffModel.enableButton == false)
+    
     }
+
+    func calculateDiff() async {
+    
+        diffModel.setButtonEnable(state: false)
+    
+        let _ : Bool = await diffModel.initWithDiff()
+    
+    }
+
 }
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
--- Shared/Differential Equation.swift
+++ Shared/Differential Equation.swift
@@ -6,14 +6,45 @@
 //
 
 import SwiftUI
+import Darwin
+
 
 class DiffEq: NSObject,ObservableObject {
 
-    var hbar = Double(pow(1.054571817, -34))
-    var m = 0.0
-    var psi = 0.0
+    var e = Darwin.M_E
+    // hbar and m are in eV
+    var hbar = Double(pow(200.0, 6))
+    var m = 0.510998950
     var i = sqrt(-1.0)
+    // h = step size
+    var h = 0.1
     var U = 0.0
+    var E = 0.0
+    var l = 0.0
+    var k = 0.0
+    var x_i = 0.0
+    var x_0 = 0.0
+    var psi_i = 0.0
+    var psi_prime = 0.0
+    var L = 2.0
+    var n = 0.0
+    var iterations = 0.0
+    var f2 = 0.0
+    var f1 = 0.0
+    var fn = 0.0
+    
+    var xPoint = 0.0
+    var psiPoint = 0.0
+    var psi_num = 0.0
+    
+    var x_array = [Double]()
+    var psi_array = [Double]()
+    var psi_prime_array = [Double]()
+    @Published var psi_String = ""
+    
+    
+    
+    
     @Published var enableButton = true
 
 
@@ -24,7 +55,7 @@ class DiffEq: NSObject,ObservableObject {
                        
                
                    
-                       taskGroup.addTask { let _ = await self.calculateDiff()}
+                       taskGroup.addTask { let _ = await self.calculateRK4()}
                    
                }
                    
@@ -39,25 +70,143 @@ class DiffEq: NSObject,ObservableObject {
 
 
 
-    func calculateDiff() async -> Double {
-// 1D Differential Schrodinger Equation:
+    func calculateRK4() async -> Double {
+        
+        
+        
+// 1D Differential Time-Independent Schrodinger Equation:
 //
 //      2            2
-//     ħ    partial Ψ(x, t)                         partial Ψ(x, t)
-// - -----  -----------------  +  U(x)Ψ(x, t)  =   iħ -------------
-//    2m                2                            partial t
+//     ħ    partial Ψ(x, t)
+// - -----  -----------------  +  U(x)Ψ(x, t)  = E(x)Ψ(x, t)
+//    2m                2
 //             partial x
 //
 //
+//                    partial Ψ
+//                             i
+//  Ψ       =  Ψ   +  h * ----------
+//   i + 1       i      partial x
+//                               i
 //
-//  psi function solution:
-//
-//                      (ikx  -  iwt)
-//        Ψ(x, t)  =  A e
 //
 //
+//
+// Runge Kutta 4:
+//
+// psi_i+1 = psi_i + h*psi_i'
+//
+// psi(0) = 0
+// psi_1 = h*psi'
+// psi_0' = guess (random number )
+//
+//
+// y_prime = f(x, Ψ)
+//
+// y(x_0) = initialCondition
+//
+// Ψ_i = Ψ_0 + i*h
+//
+//
+//
+// k1 = h*f(x_i, psi_i)
+//
+// k2 = f(x_i + h/2, w_i + h*k1/2)
+//
+// k3 = f(x_i + h/2, w_i + h*k2/2)
+//
+// k4 = f(x_i + h, w_i + h*k3)
+//
+// k + 1 = k + 1/6(k1 + 2*k2 + 2*k3 + k4)
+//
+//l + 1 = l + 1/6(l1 + 2*l2 + 2*l3 + l4)
+    
+        U = 10.0
+        E = 1.123
         
+        let f_num = (U-E)*2*m
         
+        var f2 = (f_num/(pow(hbar, 2)))*psi_i
+        
+        var f1 = psi_prime
+        var fn = ((U-E)*2*m)/(pow(hbar, 2))
+
+        
+        var k1 = 0.0
+        var k2 = 0.0
+        var k3 = 0.0
+        var k4 = 0.0
+        var l1 = 0.0
+        var l2 = 0.0
+        var l3 = 0.0
+        var l4 = 0.0
+        
+        psi_prime = Double.random(in: 0.0...1000.0)
+        l = psi_prime
+        k = psi_i
+        
+        while n < L {
+            
+           
+          
+            
+            
+        
+            
+            f1 = psi_prime
+            f2 = (f_num/(pow(hbar, 2)))*psi_i
+            k1 = f1
+            l1 = f2
+            
+            x_i = h*(x_i + h/2.0)
+            psi_i = h*(k + k1/2.0)
+            psi_prime = psi_prime + l1/2
+            f1 = psi_prime
+            f2 = (f_num/(pow(hbar, 2)))*psi_i
+            k2 = f1
+            l2 = f2
+            
+            x_i = h*(x_i + h)
+            psi_i = h*(k + k2/2.0)
+            psi_prime = psi_prime + l2/2
+            f1 = psi_prime
+            f2 = (f_num/(pow(hbar, 2)))*psi_i
+            k3 = f1
+            l3 = f2
+            
+            x_i = h*(x_i + h)
+            psi_i = h*(k + k3)
+            psi_prime = psi_prime + l3/2
+            f1 = psi_prime
+            f2 = (f_num/(pow(hbar, 2)))*psi_i
+            k4 = f1
+            l4 = f2
+            
+            x_i = x_i + iterations*h
+            k = k + (k1 + 2*k2 + 2*k3 + k4)/6
+            l = l + (l1 + 2*l2 + 2*l3 + l4)/6
+            psi_i = k
+            psi_prime = l
+// k should be close to psi_i
+// l should be close to psi_prime
+            
+            x_array.append(x_i)
+            psi_array.append(k)
+            psi_prime_array.append(l)
+       
+            
+            iterations += 1.0
+            
+            x_i = x_i + h
+            n += h
+            
+            
+        }
+        psi_i += k
+        let psi_num = psi_i
+        print("psi array:", psi_array)
+        await updatePsiString(text: "\(psi_num)")
+        return psi_i
         
     }
 
@@ -67,6 +216,24 @@ class DiffEq: NSObject,ObservableObject {
     
     
     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      @MainActor func updatePsiString(text:String){
+          
+          self.psi_String = text
+          
+      }
     
     @MainActor func setButtonEnable(state: Bool){
         
